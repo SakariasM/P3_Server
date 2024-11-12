4d9a5ad8c021334c41da0e6ae84714a3122d8c43
Sakarias <49249574+SakariasM@users.noreply.github.com>
2024-11-12 09:23:28 +0100

    Initial commit

---
+++ src/main/java/com/p3/Server/users/UsersController.java
@@ -0,0 +1,42 @@
+package com.p3.Server.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="api/user")
+public class UsersController {
+
+    private final UsersService usersService;
+
+    @Autowired      // Auto sets the userClassService as the constant
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @GetMapping        // @getmapping for endpoint of server - get denotes client request to server
+    public List<Users> getUserClasses() {
+        return usersService.getUserClasses();
+    }
+
+    @PostMapping
+    public void registerNewUser(@RequestBody Users user) {
+        usersService.addNewUser(user);
+    }
+
+    @DeleteMapping(path = "{user_id}")  // path will be api/user/"user_id"
+    public void deleteUser(@PathVariable("user_id") int user_id) {
+        usersService.deleteUser(user_id);
+    }
+
+    @PutMapping(path = "{user_id}")
+    public void updateUser(@PathVariable("user_id") int user_id,
+                           @RequestParam(required = false) String username,
+                           @RequestParam(required = false) String password,
+                           @RequestParam(required = false) String role) {
+        usersService.updateUser(user_id, username, password, role);
+    }
+
+}

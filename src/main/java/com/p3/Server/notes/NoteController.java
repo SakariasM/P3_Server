9579a1d415bf1bd45c298ecdd05afe455ae6e5c4
matiaszeller <134097344+matiaszeller@users.noreply.github.com>
2024-11-21 12:02:06 +0100

    deleted whitespace

--- src/main/java/com/p3/Server/notes/NoteController.java
+++ src/main/java/com/p3/Server/notes/NoteController.java
@@ -2,7 +2,6 @@ package com.p3.Server.notes;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
--- src/main/java/com/p3/Server/notes/NoteService.java
+++ src/main/java/com/p3/Server/notes/NoteService.java
@@ -2,7 +2,6 @@ package com.p3.Server.notes;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;

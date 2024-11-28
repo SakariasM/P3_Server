321aa654a6138a0f546cd6fe7771c8843739ed4a
Jakob <jakob.h.roenn@gmail.com>
2024-11-28 13:23:30 +0100

    Added API endpoint for adding new users!

--- src/main/java/com/p3/Server/user/UserController.java
+++ src/main/java/com/p3/Server/user/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -69,7 +70,7 @@ public class UserController {
      * POST
      */
 
-    @PostMapping
+    @PostMapping("newUser")
     public void registerNewUser(@RequestBody User user) {
         userService.addNewUser(user);
     }
